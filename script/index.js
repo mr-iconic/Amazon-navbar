b559e219dd4c197becac2938d42d9b5a64f4191a
mr-iconic <90669066+mr-iconic@users.noreply.github.com>
2022-04-13 14:38:28 +0530

    commit

---
+++ script/index.js
@@ -0,0 +1,8 @@
+function onclickmenu() {
+    var y=document.getElementById("menuoption");
+    if(y.style.display==="block"){
+        y.style.display="none";
+    }else{
+        y.style.display="block";
+    }
+}
